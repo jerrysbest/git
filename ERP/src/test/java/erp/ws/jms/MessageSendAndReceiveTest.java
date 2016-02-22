2ad29ac0f527c07a7501c45842260e9e9bfdac46
jerrysbest <jerrysbest@163.com>
2016-02-22 16:21:39 +0800

    socket test success 1

---
+++ ERP/src/test/java/erp/ws/jms/MessageSendAndReceiveTest.java
@@ -0,0 +1,14 @@
+package erp.ws.jms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageSendAndReceiveTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
